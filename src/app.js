b5881864370f1c437cefa2f9986231e60079233b
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2021-12-24 00:05:33 +0200

    Refactors the App Component

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import './index.css';
+
+import React from 'react';
+import Dropdown from './components/Dropdown';
+
+const App = () => {
+	return (
+		<React.Fragment>
+			<Dropdown />
+		</React.Fragment>
+	);
+};
+
+export default App;
