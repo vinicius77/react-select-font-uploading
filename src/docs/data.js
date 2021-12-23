768753d176023a2af68381548be41b266bb517fd
Vinicius Cerqueira Bonifacio <vinicius.bonifacio1985@gmail.com>
2021-12-23 20:55:44 +0200

    Font options array

---
+++ src/docs/data.js
@@ -0,0 +1,7 @@
+export const fontOptions = [
+	{ value: 'Arial', label: 'Arial', color: '#36B37E' },
+	{ value: 'Verdana', label: 'Verdana', color: '#00875A' },
+	{ value: 'Tahoma', label: 'Tahoma', color: '#253858' },
+	{ value: 'Impact', label: 'Impact', color: '#666666' },
+	{ value: 'Times New Roman', label: 'Times New Roman', color: '#654dab' },
+];
